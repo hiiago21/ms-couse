e4dd43a3d6b6d83704ce636e802aacf402550096
Hiago Silva <hiiago@yahoo.com.br>
2021-03-27 11:38:11 -0300

    Project created

---
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentSevice.java
@@ -0,0 +1,24 @@
+package com.devsuperior.hrpayroll.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service
+public class PaymentSevice {
+
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(Long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+	
+}
